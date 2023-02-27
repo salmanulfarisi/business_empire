e28bbf37f5ae6d58ab18faca52e4afd5371a465d
Salmanul Farisi <salmanfarisi0027@gmail.com>
2023-02-27 14:17:04 +0530

    added to wishlist done

--- lib/screen/wholesale/shop/controller/product_controller.dart
+++ lib/screen/wholesale/shop/controller/product_controller.dart
@@ -1,6 +1,7 @@
 import 'package:business_empire/screen/wholesale/shop/model/category_model.dart';
 import 'package:business_empire/screen/wholesale/shop/widgets/consts.dart';
 import 'package:business_empire/utils/firebase_consts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +9,8 @@ class ProductController extends GetxController {
   var quantity = 0.obs;
   var colorIndex = 0.obs;
   var totalPrice = 0.obs;
+
+  var isFav = false.obs;
   var subcat = [];
   getSubCategories(title) async {
     subcat.clear();
@@ -61,5 +64,27 @@ class ProductController extends GetxController {
     totalPrice.value = 0;
   }
 
-  addToWishList() async {}
+  addToWishList(docId, context) async {
+    await firestore.collection(productCollection).doc(docId).set({
+      'p_whishlist': FieldValue.arrayUnion([currentUser!.uid])
+    }, SetOptions(merge: true));
+    isFav(true);
+    VxToast.show(context, msg: "Added to Wishlist");
+  }
+
+  removeFromWishlist(docId, context) async {
+    await firestore.collection(productCollection).doc(docId).set({
+      'p_whishlist': FieldValue.arrayRemove([currentUser!.uid])
+    }, SetOptions(merge: true));
+    isFav(false);
+    VxToast.show(context, msg: "Removed from Wishlist");
+  }
+
+  checkIfFav(data) async {
+    if (data['p_whishlist'].contains(currentUser!.uid)) {
+      isFav(true);
+    } else {
+      isFav(false);
+    }
+  }
 }
--- lib/screen/wholesale/shop/view/category/item_details.dart
+++ lib/screen/wholesale/shop/view/category/item_details.dart
@@ -36,9 +36,20 @@ class ItemDetails extends StatelessWidget {
               onPressed: () {},
               icon: const Icon(Icons.share),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.favorite_outline),
+            Obx(
+              () => IconButton(
+                onPressed: () {
+                  if (controller.isFav.value) {
+                    controller.removeFromWishlist(data.id, context);
+                  } else {
+                    controller.addToWishList(data.id, context);
+                  }
+                },
+                icon: Icon(
+                  Icons.favorite_outline,
+                  color: controller.isFav.value ? redColor : darkFontGrey,
+                ),
+              ),
             ),
           ],
         ),
