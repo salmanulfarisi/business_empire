f6a5dab5fb5b3fbc5fdf181ffb3b5eb9f2ebfdd5
Salmanul Farisi <salmanfarisi0027@gmail.com>
2023-02-28 14:56:22 +0530

    seller products ui done

--- lib/screen/seller_app/view/products/seller_products.dart
+++ lib/screen/seller_app/view/products/seller_products.dart
@@ -1,11 +1,53 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:business_empire/screen/seller_app/const/seller_const.dart';
+import 'package:business_empire/screen/seller_app/view/widgets/text_style.dart';
+import 'package:intl/intl.dart' as intl;
 
 class SellerProducts extends StatelessWidget {
   const SellerProducts({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: boldText(text: products, color: fontGrey, size: 16.0),
+        actions: [
+          Center(
+            child: normalText(
+                text: intl.DateFormat('EEE,MMM d,' 'yy').format(DateTime.now()),
+                color: darkGrey,
+                size: 16.0),
+          ),
+          10.widthBox,
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: List.generate(
+                20,
+                (index) => ListTile(
+                      onTap: () {},
+                      leading: Image.asset(
+                        imgProduct,
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      title: boldText(
+                          text: 'Product Name', size: 16.0, color: fontGrey),
+                      subtitle: normalText(text: "\$40.0", color: darkGrey),
+                    )),
+          ),
+        ),
+      ),
+    );
   }
 }
