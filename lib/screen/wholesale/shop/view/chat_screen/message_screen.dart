a242c3caf671cc5e96d29423c9387e2a16bb0fbc
Salmanul Farisi <salmanfarisi0027@gmail.com>
2023-02-28 10:25:12 +0530

    all messages page done

--- lib/screen/wholesale/shop/view/chat_screen/message_screen.dart
+++ lib/screen/wholesale/shop/view/chat_screen/message_screen.dart
@@ -1,7 +1,9 @@
 import 'package:business_empire/screen/wholesale/shop/services/firestore_sercices.dart';
+import 'package:business_empire/screen/wholesale/shop/view/chat_screen/chat_screen.dart';
 import 'package:business_empire/screen/wholesale/shop/widgets/consts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MessagesScreen extends StatelessWidget {
   const MessagesScreen({Key? key}) : super(key: key);
@@ -26,7 +28,44 @@ class MessagesScreen extends StatelessWidget {
           } else if (snapshot.data!.docs.isEmpty) {
             return "No Messages Yet".text.color(darkFontGrey).makeCentered();
           } else {
-            return Container();
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            onTap: () {
+                              Get.to(() => const ChatScreen(), arguments: [
+                                data[index]['friend_name'],
+                                data[index]['toId']
+                              ]);
+                            },
+                            leading: const CircleAvatar(
+                              backgroundColor: redColor,
+                              child: Icon(
+                                Icons.person,
+                                color: whiteColor,
+                              ),
+                            ),
+                            title: "${data[index]['friend_name']}"
+                                .text
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .make(),
+                            subtitle: "${data[index]['last_msg']}".text.make(),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            );
           }
         },
       ),
